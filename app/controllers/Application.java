0a218e9abedd63bb3625b9db5bb6446720441851
nazmid <nazmidemirci87@gmail.com>
2013-06-08 22:25:02 +0300

    Dashboard

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,15 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import models.Project;
+import models.Task;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
-  
-    public static Result index() {
-        return ok(index.render("ZenTasks will be here"));
-    }
-  
+
+	public static Result index() {
+		return ok(index.render(Project.find.all(), Task.find.all()));
+	}
+
 }
